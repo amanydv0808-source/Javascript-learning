971b705bf1935617b3fba2ec38ecf95d58fb4547
amanydv0808-source <amanydv0808@gmail.com>
2026-09-24 14:34:15 +0000

    additon some more code

--- 01_basic/03_operationconversion.js
+++ 01_basic/03_operationconversion.js
@@ -10,17 +10,67 @@ let scorenumber=Number(score)
 //"33abc"=> string not converted into numbertype ouput=>NaN
 //"true"=>1 ,"false"=0
 
-let isloggedin= "Aman"
-let inbooleanloggedin=Boolean(isloggedin)
-console.log(inbooleanloggedin)
+// let isloggedin= "Aman"
+// let inbooleanloggedin=Boolean(isloggedin)
+// console.log(inbooleanloggedin)
 
 //"aman"=>true
 //""=>false
 //1=>true ,0=>false
 
 //number to string
-let somenumber=33
-let numberInString=String(somenumber)
-console.log(numberInString)
-console.log(typeof numberInString )
+// let somenumber=33
+// let numberInString=String(somenumber)
+// console.log(numberInString)
+// console.log(typeof numberInString )
+
+// **********************OPERATIONS**********************
+
+// let value=3
+// let negvalue=-value
+// console.log(negvalue)
+
+// console.log(4+5)
+// console.log(5-1)
+// console.log(4%5)
+// console.log(4*5)
+// console.log(4/5)
+// console.log(4**5)
+// console.log(2**3)
+
+// let str1="Aman"
+// let str2=" Yadav"
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log(1+2)
+// console.log("1"+2)/*  in both the cases number is converted into string */
+// console.log(1+"2")
+
+// console.log("1"+2+2)// dono case  mai pehle string aa ja rha hai toh uske baad wale, 
+// console.log(1+"2"+2)//number string mai convert ho jayega 
+// console.log(1+2+"2") //pehle addition perform hoga phir string mai convert hoga 
+
+
+// console.log(true)// output=>true
+// console.log(+true)//output+=>1 //not good practice to write code like this reduces readability
+// let str1= ""
+// let isboolean=Boolean(str1)
+// console.log(isboolean)//output=>false
+// console.log(+"")// =>0
+
+let gamecounter=100
+console.log(gamecounter)
+// let x= gamecounter++
+// let y=++gamecounter
+// console.log(gamecounter)
+// console.log(x)
+// console.log(y)
+let z=gamecounter--
+let w=--gamecounter
+console.log(z)
+console.log(w)
+
+
+
 
